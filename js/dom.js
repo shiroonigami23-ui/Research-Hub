3a2d9ecdadeb1b4f26c9029cfca6d5e3a214689d
shiroonigami23-ui <shiroonigami23@gmail.com>
2025-10-20 07:59:59 +0530

    Update dom.js

--- js/dom.js
+++ js/dom.js
@@ -31,6 +31,14 @@ export const elements = {
     editProjectId: document.getElementById('edit-project-id'),
     editProjectName: document.getElementById('edit-project-name'),
     cancelEditProject: document.getElementById('cancel-edit-project'),
+    
+    // NEW: Confirmation Modal elements
+    confirmationModal: document.getElementById('confirmation-modal'),
+    confirmationTitle: document.getElementById('confirmation-title'),
+    confirmationMessage: document.getElementById('confirmation-message'),
+    cancelConfirmation: document.getElementById('cancel-confirmation'),
+    confirmAction: document.getElementById('confirm-action'),
+
 
     // Notifications
     notification: document.getElementById('notification'),
@@ -112,7 +120,7 @@ export function renderResources(filteredResources, totalResourcesInProject, sear
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" /></svg>
                      </button>
                      <button class="icon-btn delete-resource-btn" data-id="${resource.id}" aria-label="Delete Resource">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a4.8108 4.8108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.134-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.067-2.09 1.02-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" /></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.134-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.067-2.09 1.02-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" /></svg>
                      </button>
                 </div>
             `;
@@ -149,4 +157,23 @@ export function closeModal(modal) {
 export function closeAllModals() {
     closeModal(elements.editResourceModal);
     closeModal(elements.editProjectModal);
+    closeModal(elements.confirmationModal);
+}
+
+// NEW: Reusable confirmation modal function
+export function showConfirmation({ title, message, onConfirm }) {
+    elements.confirmationTitle.textContent = title;
+    elements.confirmationMessage.textContent = message;
+
+    // We need to clone and replace the button to remove old event listeners
+    const newConfirmBtn = elements.confirmAction.cloneNode(true);
+    elements.confirmAction.parentNode.replaceChild(newConfirmBtn, elements.confirmAction);
+    elements.confirmAction = newConfirmBtn; // Update the reference in our elements object
+
+    elements.confirmAction.addEventListener('click', () => {
+        onConfirm();
+        closeModal(elements.confirmationModal);
+    });
+
+    openModal(elements.confirmationModal);
 }
